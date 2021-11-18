d5504b133e6c2166391c755e513f7daa3f9cb884
Pooglefoot <pooglefoot@gmail.com>
2021-11-18 19:39:16 +0100

    #5 - Minor deletion of deprecated dependency in bootstrap file.

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -4,9 +4,6 @@ const PROJECT_ROOT_PATH = __DIR__ . "/../";
 // Include Main Config
 require_once PROJECT_ROOT_PATH . "/inc/config.php";
 
-// Include Base Controller
-require_once PROJECT_ROOT_PATH . "/Controller/BaseController.php";
-
 // Include UserModel
 require_once PROJECT_ROOT_PATH . "/Model/UserModel.php";
 ?>
